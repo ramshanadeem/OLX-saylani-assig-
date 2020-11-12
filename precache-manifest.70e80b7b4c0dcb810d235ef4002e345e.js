96f3fa1cd3957e7281ad879d11233b4dc8db9d52
ramshanadeem <61450981+ramshanadeem@users.noreply.github.com>
2020-11-12 22:22:23 +0500

    Add files via upload

---
+++ precache-manifest.70e80b7b4c0dcb810d235ef4002e345e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10817d7d40b128724ff0731b508bd619",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d35a0f6c4fa717f8cdf",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e6d5983c0826a4e63ef9",
+    "url": "/static/css/main.8accf360.chunk.css"
+  },
+  {
+    "revision": "8d35a0f6c4fa717f8cdf",
+    "url": "/static/js/2.c880a732.chunk.js"
+  },
+  {
+    "revision": "3619d5503a72ec9f1a29a4c743775f2e",
+    "url": "/static/js/2.c880a732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d5983c0826a4e63ef9",
+    "url": "/static/js/main.747d425c.chunk.js"
+  },
+  {
+    "revision": "a583984207e240353542",
+    "url": "/static/js/runtime-main.7b36a6f8.js"
+  },
+  {
+    "revision": "ae85a01f17f64827858c3fb6d2a817d4",
+    "url": "/static/media/adds.ae85a01f.png"
+  },
+  {
+    "revision": "5d87588fef9d21913aa97962a39efab5",
+    "url": "/static/media/olxlogo.5d87588f.png"
+  },
+  {
+    "revision": "232221ab0d39ba98a9e730340dec9f08",
+    "url": "/static/media/phone.232221ab.png"
+  },
+  {
+    "revision": "20a83a1f0af5c915d3781e55bb95d833",
+    "url": "/static/media/phone2.20a83a1f.jpg"
+  },
+  {
+    "revision": "460bd8bf4d7faaedd30ff5ddacccf3cd",
+    "url": "/static/media/staticmap.460bd8bf.png"
+  }
+]);
